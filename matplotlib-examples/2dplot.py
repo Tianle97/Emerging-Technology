13c3008bb7d1af07757a70e6d631ad16295026c7
tianle97 <386466921@qq.com>
2018-10-13 17:07:20 +0100

    Update-Two plots on one set of axes

--- matplotlib-examples/2dplot.py
+++ matplotlib-examples/2dplot.py
@@ -1,5 +1,18 @@
 import matplotlib.pyplot as plt 
+import numpy as np
 
-plt.plot([1, 2, 3, 4],[1, 4, 9, 16],'b.')
+x = np.arange(0.0, 10.0, 0.01)
+y = 3.0 * x + 1.0
+
+noise = np.random.normal(0.0, 1.0, len(x))
+
+plt.plot(x, y + noise, 'r.')
+plt.plot(x, y, 'b-')
 plt.ylabel('some numbers')
+plt.show()
+
+plt.plot(x, y + noise, 'c.')
+plt.plot(x, y, 'g-')
+plt.ylabel('some numbers')
+
 plt.show()
